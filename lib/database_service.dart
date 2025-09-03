1472a5cd721435bec5db973e3f2b5aac137ac2a3
mustafa-fahimi <mostafafahimi73@gmail.com>
2025-09-03 15:56:07 +0330

    feat: implement Hive and Secure Storage services with encryption support

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -3,8 +3,8 @@ library;
 
 export 'common/database_service_exception.dart';
 export 'common/job_done.dart';
-export 'no_sql/hive/hive_service.dart';
-export 'no_sql/objectbox/objectbox_service.dart';
-export 'no_sql/secure_storage/secure_storage_service.dart';
+export 'nosql/hive/hive_service.dart';
+export 'nosql/objectbox/objectbox_service.dart';
+export 'nosql/secure_storage/secure_storage_service.dart';
 export 'sql/drift/drift_service.dart';
 export 'sql/sqflite/sqflite_service.dart';
